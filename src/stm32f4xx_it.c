cca0f3c401145dad21172b0e486fa776475ace88
Mbele Lebohang <micheallm95@gmail.com>
2017-11-05 10:54:13 +0200

    Apples

--- src/stm32f4xx_it.c
+++ src/stm32f4xx_it.c
@@ -1,36 +1,31 @@
 /**
   ******************************************************************************
-  * @file    Project/STM32F4xx_StdPeriph_Template/stm32f4xx_it.c 
+  * @file    Audio_playback_and_record/src/stm32f4xx_it.c 
   * @author  MCD Application Team
-  * @version V1.1.0
-  * @date    18-January-2013
+  * @version V1.0.0
+  * @date    28-October-2011
   * @brief   Main Interrupt Service Routines.
   *          This file provides template for all exceptions handler and 
   *          peripherals interrupt service routine.
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; COPYRIGHT 2013 STMicroelectronics</center></h2>
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
   *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        http://www.st.com/software_license_agreement_liberty_v2
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
+  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
   ******************************************************************************
-  */
+  */ 
 
 /* Includes ------------------------------------------------------------------*/
-#include "stm32f4xx_it.h"
+#include "main.h"
 
-/** @addtogroup Template_Project
+
+/** @addtogroup STM32F4-Discovery_Audio_Player_Recorder
   * @{
   */
 
@@ -38,6 +33,22 @@
 /* Private define ------------------------------------------------------------*/
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
+__IO uint8_t PauseResumeStatus = 2, Count = 0, LED_Toggle = 0;
+uint16_t capture = 0;
+extern __IO uint16_t CCR_Val;
+extern __IO uint8_t RepeatState, AudioPlayStart;
+extern uint8_t Buffer[];
+
+#if defined MEDIA_USB_KEY
+__IO uint16_t Time_Rec_Base = 0;
+ extern USB_OTG_CORE_HANDLE          USB_OTG_Core;
+ extern USBH_HOST                    USB_Host;
+ extern FIL file;
+ extern __IO uint8_t Data_Status;
+ extern __IO uint32_t XferCplt ;
+ extern __IO uint8_t Command_index;
+#endif /* MEDIA_USB_KEY */
+
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
 
@@ -140,15 +151,164 @@ void PendSV_Handler(void)
   */
 void SysTick_Handler(void)
 {
-/*  TimingDelay_Decrement(); */
+  TimingDelay_Decrement();
+#if defined MEDIA_USB_KEY
+  if ( Command_index == 1)
+  {
+    Time_Rec_Base ++;
+  }
+#endif
 }
+
 
 /******************************************************************************/
 /*                 STM32F4xx Peripherals Interrupt Handlers                   */
 /*  Add here the Interrupt Handler for the used peripheral(s) (PPP), for the  */
 /*  available peripheral interrupt handler's name please refer to the startup */
-/*  file (startup_stm32f40xx.s/startup_stm32f427x.s).                         */
+/*  file (startup_stm32f4xx.s).                                               */
 /******************************************************************************/
+/**
+  * @brief  This function handles External line 1 interrupt request.
+  * @param  None
+  * @retval None
+  */
+void EXTI1_IRQHandler(void)
+{
+  /* Check the clic on the accelerometer to Pause/Resume Playing */
+  if(EXTI_GetITStatus(EXTI_Line1) != RESET)
+  {
+    if( Count==1)
+    {
+      PauseResumeStatus = 1;
+      Count = 0;
+    }
+    else
+    {
+      PauseResumeStatus = 0;
+      Count = 1;
+    }
+    /* Clear the EXTI line 1 pending bit */
+    EXTI_ClearITPendingBit(EXTI_Line1);
+  }
+}
+
+/**
+  * @brief  This function handles TIM4 global interrupt request.
+  * @param  None
+  * @retval None
+  */
+void TIM4_IRQHandler(void)
+{
+   uint8_t clickreg = 0;
+
+  if (AudioPlayStart != 0x00)
+  {
+    /* Read click status register */
+    LIS302DL_Read(&clickreg, LIS302DL_CLICK_SRC_REG_ADDR, 1); 
+    LIS302DL_Read(Buffer, LIS302DL_STATUS_REG_ADDR, 6);
+  }
+  
+  /* Checks whether the TIM interrupt has occurred */
+  if (TIM_GetITStatus(TIM4, TIM_IT_CC1) != RESET)
+  {
+    TIM_ClearITPendingBit(TIM4, TIM_IT_CC1);
+    if( LED_Toggle == 3)
+    {
+      /* LED3 Orange toggling */
+      STM_EVAL_LEDToggle(LED3);
+      STM_EVAL_LEDOff(LED6);
+      STM_EVAL_LEDOff(LED4);
+    }
+    else if( LED_Toggle == 4)
+    {
+      /* LED4 Green toggling */
+      STM_EVAL_LEDToggle(LED4);
+      STM_EVAL_LEDOff(LED6);
+      STM_EVAL_LEDOff(LED3);
+    }
+    else if( LED_Toggle == 6)
+    {
+      /* LED6 Blue toggling */
+      STM_EVAL_LEDOff(LED3);
+      STM_EVAL_LEDOff(LED4);
+      STM_EVAL_LEDToggle(LED6);
+    }
+    else if (LED_Toggle ==0)
+    {
+      /* LED6 Blue On to signal Pause */
+      STM_EVAL_LEDOn(LED6);
+    }
+    else if (LED_Toggle == 7)
+    {
+      /* LED4 toggling with frequency = 439.4 Hz */
+      STM_EVAL_LEDOff(LED3);
+      STM_EVAL_LEDOff(LED4);
+      STM_EVAL_LEDOff(LED5);
+      STM_EVAL_LEDOff(LED6);
+    }
+    capture = TIM_GetCapture1(TIM4);
+    TIM_SetCompare1(TIM4, capture + CCR_Val);
+  }
+}
+
+#if defined MEDIA_USB_KEY
+/**
+  * @brief  EXTI0_IRQHandler
+  *         This function handles External line 0 interrupt request.
+  * @param  None
+  * @retval None
+  */
+void EXTI0_IRQHandler(void)
+{
+  /* Checks whether the User Button EXTI line is asserted*/
+  if (EXTI_GetITStatus(EXTI_Line0) != RESET) 
+  { 
+    if (Command_index == 1)
+    {
+      RepeatState = 0;
+      /* Switch to play command */
+      Command_index = 0;
+    }
+    else if (Command_index == 0)
+    {
+      /* Switch to record command */
+      Command_index = 1;
+      XferCplt = 1;
+      EVAL_AUDIO_Stop(CODEC_PDWN_SW);
+    }
+    else
+    {
+      RepeatState = 0;
+      /* Switch to play command */
+      Command_index = 0; 
+    }
+  } 
+  /* Clears the EXTI's line pending bit.*/ 
+  EXTI_ClearITPendingBit(EXTI_Line0);
+}
+
+
+/**
+  * @brief  This function handles TIM2 global interrupt request.
+  * @param  None
+  * @retval None
+  */
+void TIM2_IRQHandler(void)
+{
+  USB_OTG_BSP_TimerIRQ();
+}
+
+
+/**
+  * @brief  This function handles USB-On-The-Go FS global interrupt request.
+  * @param  None
+  * @retval None
+  */
+void OTG_FS_IRQHandler(void)
+{
+  USBH_OTG_ISR_Handler(&USB_OTG_Core);
+}
+#endif /* MEDIA_USB_KEY */
 
 /**
   * @brief  This function handles PPP interrupt request.
@@ -159,9 +319,9 @@ void SysTick_Handler(void)
 {
 }*/
 
+
 /**
   * @}
   */ 
-
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+  
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
---
+++ src/waveplayer.c
@@ -0,0 +1,439 @@
+/**
+  ******************************************************************************
+  * @file    Audio_playback_and_record/src/waveplayer.c 
+  * @author  MCD Application Team
+  * @version V1.0.0
+  * @date    28-October-2011
+  * @brief   I2S audio program 
+  ******************************************************************************
+  * @attention
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+  *
+  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/** @addtogroup STM32F4-Discovery_Audio_Player_Recorder
+* @{
+*/ 
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+#if defined MEDIA_IntFLASH
+ /* This is an audio file stored in the Flash memory as a constant table of 16-bit data.
+    The audio format should be WAV (raw / PCM) 16-bits, Stereo (sampling rate may be modified) */
+extern uint16_t AUDIO_SAMPLE[];
+/* Audio file size and start address are defined here since the audio file is 
+    stored in Flash memory as a constant table of 16-bit data */
+#define AUDIO_FILE_SZE          990000
+#define AUIDO_START_ADDRESS     58 /* Offset relative to audio file header size */
+#endif
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+
+
+__IO uint32_t XferCplt = 0;
+__IO uint8_t volume = 70, AudioPlayStart = 0;
+__IO uint32_t WaveCounter;
+uint8_t Buffer[6];
+__IO uint32_t WaveDataLength = 0;
+extern __IO uint8_t Count;
+extern __IO uint8_t RepeatState ;
+extern __IO uint8_t LED_Toggle;
+extern __IO uint8_t PauseResumeStatus ;
+extern uint32_t AudioRemSize; 
+static __IO uint32_t TimingDelay;
+
+/* Private function prototypes -----------------------------------------------*/
+static void Mems_Config(void);
+static void EXTILine_Config(void);
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Play wave from a mass storge
+  * @param  AudioFreq: Audio Sampling Frequency
+  * @retval None
+*/
+
+void WavePlayBack(uint32_t AudioFreq)
+{ 
+  /* 
+  Normal mode description:
+  Start playing the audio file (using DMA stream) .
+  Using this mode, the application can run other tasks in parallel since 
+  the DMA is handling the Audio Transfer instead of the CPU.
+  The only task remaining for the CPU will be the management of the DMA 
+  Transfer Complete interrupt or the Half Transfer Complete interrupt in 
+  order to load again the buffer and to calculate the remaining data.  
+  Circular mode description:
+  Start playing the file from a circular buffer, once the DMA is enabled it 
+  always run. User has to fill periodically the buffer with the audio data 
+  using Transfer complete and/or half transfer complete interrupts callbacks 
+  (EVAL_AUDIO_TransferComplete_CallBack() or EVAL_AUDIO_HalfTransfer_CallBack()...
+  In this case the audio data file is smaller than the DMA max buffer 
+  size 65535 so there is no need to load buffer continuously or manage the 
+  transfer complete or Half transfer interrupts callbacks. */  
+  
+  /* Start playing */
+  AudioPlayStart = 1;
+  RepeatState =0;
+#if defined MEDIA_IntFLASH 
+  
+  /* Initialize wave player (Codec, DMA, I2C) */
+  WavePlayerInit(AudioFreq);
+  
+  /* Play on */
+  AudioFlashPlay((uint16_t*)(AUDIO_SAMPLE + AUIDO_START_ADDRESS),AUDIO_FILE_SZE,AUIDO_START_ADDRESS);
+  
+#endif 
+
+}
+
+/**
+  * @brief  Pause or Resume a played wave
+  * @param  state: if it is equal to 0 pause Playing else resume playing
+  * @retval None
+  */
+void WavePlayerPauseResume(uint8_t state)
+{ 
+  EVAL_AUDIO_PauseResume(state);   
+}
+
+/**
+  * @brief  Configure the volune
+  * @param  vol: volume value
+  * @retval None
+  */
+uint8_t WaveplayerCtrlVolume(uint8_t vol)
+{ 
+  EVAL_AUDIO_VolumeCtl(vol);
+  return 0;
+}
+
+
+/**
+  * @brief  Stop playing wave
+  * @param  None
+  * @retval None
+  */
+void WavePlayerStop(void)
+{ 
+  EVAL_AUDIO_Stop(CODEC_PDWN_SW);
+}
+ 
+/**
+* @brief  Initializes the wave player
+* @param  AudioFreq: Audio sampling frequency
+* @retval None
+*/
+int WavePlayerInit(uint32_t AudioFreq)
+{ 
+  
+  /* MEMS Accelerometre configure to manage PAUSE, RESUME and Controle Volume operation */
+  Mems_Config();
+  
+  /* EXTI configue to detect interrupts on Z axis click and on Y axis high event */
+  EXTILine_Config();  
+    
+  /* Initialize I2S interface */  
+  EVAL_AUDIO_SetAudioInterface(AUDIO_INTERFACE_I2S);
+  
+  /* Initialize the Audio codec and all related peripherals (I2S, I2C, IOExpander, IOs...) */  
+  EVAL_AUDIO_Init(OUTPUT_DEVICE_AUTO, volume, AudioFreq );  
+  
+  return 0;
+}
+
+/**
+  * @brief  MEMS accelerometre management of the timeout situation.
+  * @param  None.
+  * @retval None.
+  */
+uint32_t LIS302DL_TIMEOUT_UserCallback(void)
+{
+  /* MEMS Accelerometer Timeout error occured */
+  while (1)
+  {   
+  }
+}
+
+/**
+* @brief  Play wave file from internal Flash
+* @param  None
+* @retval None
+*/
+uint32_t AudioFlashPlay(uint16_t* pBuffer, uint32_t FullSize, uint32_t StartAdd)
+{ 
+  EVAL_AUDIO_Play((uint16_t*)pBuffer, (FullSize - StartAdd));
+  return 0;
+}
+
+/*--------------------------------
+Callbacks implementation:
+the callbacks prototypes are defined in the stm324xg_eval_audio_codec.h file
+and their implementation should be done in the user code if they are needed.
+Below some examples of callback implementations.
+--------------------------------------------------------*/
+/**
+* @brief  Calculates the remaining file size and new position of the pointer.
+* @param  None
+* @retval None
+*/
+void EVAL_AUDIO_TransferComplete_CallBack(uint32_t pBuffer, uint32_t Size)
+{
+  /* Calculate the remaining audio data in the file and the new size 
+  for the DMA transfer. If the Audio files size is less than the DMA max 
+  data transfer size, so there is no calculation to be done, just restart 
+  from the beginning of the file ... */
+  /* Check if the end of file has been reached */
+  
+#ifdef AUDIO_MAL_MODE_NORMAL  
+  
+#if defined MEDIA_IntFLASH
+
+#if defined PLAY_REPEAT_OFF
+  LED_Toggle = 4;
+  RepeatState = 1;
+  EVAL_AUDIO_Stop(CODEC_PDWN_HW);
+#else
+  /* Replay from the beginning */
+  AudioFlashPlay((uint16_t*)(AUDIO_SAMPLE + AUIDO_START_ADDRESS),AUDIO_FILE_SZE,AUIDO_START_ADDRESS);
+#endif  
+  
+#elif defined MEDIA_USB_KEY  
+  XferCplt = 1;
+  if (WaveDataLength) WaveDataLength -= _MAX_SS;
+  if (WaveDataLength < _MAX_SS) WaveDataLength = 0;
+    
+#endif 
+    
+#else /* #ifdef AUDIO_MAL_MODE_CIRCULAR */
+  
+  
+#endif /* AUDIO_MAL_MODE_CIRCULAR */
+}
+
+/**
+* @brief  Manages the DMA Half Transfer complete interrupt.
+* @param  None
+* @retval None
+*/
+void EVAL_AUDIO_HalfTransfer_CallBack(uint32_t pBuffer, uint32_t Size)
+{  
+#ifdef AUDIO_MAL_MODE_CIRCULAR
+    
+#endif /* AUDIO_MAL_MODE_CIRCULAR */
+  
+  /* Generally this interrupt routine is used to load the buffer when 
+  a streaming scheme is used: When first Half buffer is already transferred load 
+  the new data to the first half of buffer while DMA is transferring data from 
+  the second half. And when Transfer complete occurs, load the second half of 
+  the buffer while the DMA is transferring from the first half ... */
+  /* 
+  ...........
+  */
+}
+
+/**
+* @brief  Manages the DMA FIFO error interrupt.
+* @param  None
+* @retval None
+*/
+void EVAL_AUDIO_Error_CallBack(void* pData)
+{
+  /* Stop the program with an infinite loop */
+  while (1)
+  {}
+  
+  /* could also generate a system reset to recover from the error */
+  /* .... */
+}
+
+/**
+* @brief  Get next data sample callback
+* @param  None
+* @retval Next data sample to be sent
+*/
+uint16_t EVAL_AUDIO_GetSampleCallBack(void)
+{
+  return 0;
+}
+
+
+#ifndef USE_DEFAULT_TIMEOUT_CALLBACK
+/**
+  * @brief  Basic management of the timeout situation.
+  * @param  None.
+  * @retval None.
+  */
+uint32_t Codec_TIMEOUT_UserCallback(void)
+{   
+  return (0);
+}
+#endif /* USE_DEFAULT_TIMEOUT_CALLBACK */
+/*----------------------------------------------------------------------------*/
+
+/**
+  * @brief  Inserts a delay time.
+  * @param  nTime: specifies the delay time length, in 10 ms.
+  * @retval None
+  */
+void Delay(__IO uint32_t nTime)
+{
+  TimingDelay = nTime;
+  
+  while(TimingDelay != 0);
+}
+
+/**
+  * @brief  Decrements the TimingDelay variable.
+  * @param  None
+  * @retval None
+  */
+void TimingDelay_Decrement(void)
+{
+  if (TimingDelay != 0x00)
+  { 
+    TimingDelay--;
+  }
+}
+
+/**
+* @brief  configure the mems accelometer
+* @param  None
+* @retval None
+*/
+static void Mems_Config(void)
+{ 
+  uint8_t ctrl = 0;
+  
+  LIS302DL_InitTypeDef  LIS302DL_InitStruct;
+  LIS302DL_InterruptConfigTypeDef LIS302DL_InterruptStruct;  
+  
+  /* Set configuration of LIS302DL*/
+  LIS302DL_InitStruct.Power_Mode = LIS302DL_LOWPOWERMODE_ACTIVE;
+  LIS302DL_InitStruct.Output_DataRate = LIS302DL_DATARATE_100;
+  LIS302DL_InitStruct.Axes_Enable = LIS302DL_X_ENABLE | LIS302DL_Y_ENABLE | LIS302DL_Z_ENABLE;
+  LIS302DL_InitStruct.Full_Scale = LIS302DL_FULLSCALE_2_3;
+  LIS302DL_InitStruct.Self_Test = LIS302DL_SELFTEST_NORMAL;
+  LIS302DL_Init(&LIS302DL_InitStruct);
+    
+  /* Set configuration of Internal High Pass Filter of LIS302DL*/
+  LIS302DL_InterruptStruct.Latch_Request = LIS302DL_INTERRUPTREQUEST_LATCHED;
+  LIS302DL_InterruptStruct.SingleClick_Axes = LIS302DL_CLICKINTERRUPT_Z_ENABLE;
+  LIS302DL_InterruptStruct.DoubleClick_Axes = LIS302DL_DOUBLECLICKINTERRUPT_Z_ENABLE;
+  LIS302DL_InterruptConfig(&LIS302DL_InterruptStruct);
+  
+  /* Configure Interrupt control register: enable Click interrupt on INT1 and
+     INT2 on Z axis high event */
+  ctrl = 0x3F;
+  LIS302DL_Write(&ctrl, LIS302DL_CTRL_REG3_ADDR, 1);
+  
+  /* Enable Interrupt generation on click on Z axis */
+  ctrl = 0x50;
+  LIS302DL_Write(&ctrl, LIS302DL_CLICK_CFG_REG_ADDR, 1);
+  
+  /* Configure Click Threshold on X/Y axis (10 x 0.5g) */
+  ctrl = 0xAA;
+  LIS302DL_Write(&ctrl, LIS302DL_CLICK_THSY_X_REG_ADDR, 1);
+  
+  /* Configure Click Threshold on Z axis (10 x 0.5g) */
+  ctrl = 0x0A;
+  LIS302DL_Write(&ctrl, LIS302DL_CLICK_THSZ_REG_ADDR, 1);
+  
+  /* Enable interrupt on Y axis high event */
+  ctrl = 0x4C;
+  LIS302DL_Write(&ctrl, LIS302DL_FF_WU_CFG1_REG_ADDR, 1);
+  
+  /* Configure Time Limit */
+  ctrl = 0x03;
+  LIS302DL_Write(&ctrl, LIS302DL_CLICK_TIMELIMIT_REG_ADDR, 1);
+    
+  /* Configure Latency */
+  ctrl = 0x7F;
+  LIS302DL_Write(&ctrl, LIS302DL_CLICK_LATENCY_REG_ADDR, 1);
+  
+  /* Configure Click Window */
+  ctrl = 0x7F;
+  LIS302DL_Write(&ctrl, LIS302DL_CLICK_WINDOW_REG_ADDR, 1);
+    
+}
+
+/**
+  * @brief  Configures EXTI Line0 (connected to PA0 pin) in interrupt mode
+  * @param  None
+  * @retval None
+  */
+static void EXTILine_Config(void)
+{
+  GPIO_InitTypeDef   GPIO_InitStructure;
+  NVIC_InitTypeDef   NVIC_InitStructure;
+  EXTI_InitTypeDef   EXTI_InitStructure;
+  /* Enable GPIOA clock */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+  /* Enable SYSCFG clock */
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+  /* Configure PE0 and PE1 pins as input floating */
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0|GPIO_Pin_1;
+  GPIO_Init(GPIOE, &GPIO_InitStructure);
+
+  /* Connect EXTI Line to PE1 pins */
+  SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE, EXTI_PinSource1);
+
+  /* Configure EXTI Line1 */
+  EXTI_InitStructure.EXTI_Line = EXTI_Line1;
+  EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+  EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;  
+  EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+  EXTI_Init(&EXTI_InitStructure);
+
+  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_3);
+  
+  /* Enable and set EXTI Line0 Interrupt to the lowest priority */
+  NVIC_InitStructure.NVIC_IRQChannel = EXTI1_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x00;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x00;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+* @brief  Reports the name of the source file and the source line number
+*   where the assert_param error has occurred.
+* @param  file: pointer to the source file name
+* @param  line: assert_param error line source number
+* @retval None
+*/
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+  ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+* @}
+*/ 
+
+
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
